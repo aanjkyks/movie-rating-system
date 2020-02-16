cd55bea672523448e2ce75840be7e8dfda481c35
Ainis Antans <ainis.antans@gmail.com>
2020-02-16 12:36:51 +0200

    add cors configuration

---
+++ src/main/kotlin/com/herokuapp/movieratingsystem/security/config/SecurityConfig.kt
@@ -0,0 +1,36 @@
+package com.herokuapp.movieratingsystem.security.config
+
+//import com.herokuapp.movieratingsystem.security.filter.JwtRequestFilter
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+
+//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+
+
+@EnableWebSecurity
+@Configuration
+class SecurityConfig(private val userDetailsService: UserDetailsService) : WebSecurityConfigurerAdapter() {
+
+
+    override fun configure(http: HttpSecurity) {
+        http.cors()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource? {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("*")
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "OPTIONS")
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+}
