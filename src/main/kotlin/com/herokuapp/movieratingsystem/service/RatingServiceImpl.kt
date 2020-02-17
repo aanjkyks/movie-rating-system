92b81957601332078ded4604d001f3549725decd
Ainis Antans <ainis.antans@gmail.com>
2020-02-17 22:10:22 +0200

    average ratings to allow sorting by it (#18)
    * fixed average rating for movies
    
    * fixed average rating for movies (I hope)

--- src/main/kotlin/com/herokuapp/movieratingsystem/service/RatingServiceImpl.kt
+++ src/main/kotlin/com/herokuapp/movieratingsystem/service/RatingServiceImpl.kt
@@ -7,11 +7,16 @@ import com.herokuapp.movieratingsystem.repository.RatingRepository
 import org.springframework.stereotype.Service
 
 @Service
-class RatingServiceImpl(private val ratingRepository: RatingRepository) : RatingService {
+class RatingServiceImpl(private val ratingRepository: RatingRepository,
+                        private val movieService: MovieService) : RatingService {
     override fun saveRating(rating: Rating): Rating {
-        if (rating.value > 10) {
-            throw MVRInvalidArgumentException("Rating can not exceed 10")
+        if (rating.value > 10 || rating.value < 0) {
+            throw MVRInvalidArgumentException("Rating can only be in range 0 - 10")
         }
+        val ratings = mutableListOf(rating)
+        ratings.addAll(rating.movie.ratings)
+        rating.movie.avgRating = ratings.map { it.value }.sum().div(ratings.size.toDouble())
+        movieService.saveMovie(rating.movie)
         return ratingRepository.save(rating)
     }
 }
--- src/test/kotlin/com/herokuapp/movieratingsystem/service/RatingServiceImplTest.kt
+++ src/test/kotlin/com/herokuapp/movieratingsystem/service/RatingServiceImplTest.kt
@@ -1,6 +1,7 @@
 package com.herokuapp.movieratingsystem.service
 
 import com.herokuapp.movieratingsystem.exceptions.MVRInvalidArgumentException
+import com.herokuapp.movieratingsystem.model.Movie
 import com.herokuapp.movieratingsystem.model.Rating
 import com.herokuapp.movieratingsystem.repository.RatingRepository
 import com.nhaarman.mockitokotlin2.*
@@ -10,14 +11,18 @@ import org.junit.jupiter.api.assertThrows
 internal class RatingServiceImplTest {
 
     val ratingRepository: RatingRepository = mock()
+    val movieService: MovieService = mock()
 
-    val ratingService: RatingService = RatingServiceImpl(ratingRepository)
+    val ratingService: RatingService = RatingServiceImpl(ratingRepository, movieService)
 
     @Test
     fun saveRating() {
         whenever(ratingRepository.save(any<Rating>())).thenReturn(Rating())
-        ratingService.saveRating(Rating())
+        val rating = Rating()
+        rating.movie = Movie()
+        ratingService.saveRating(rating)
         verify(ratingRepository, times(1)).save(any<Rating>())
+        verify(movieService).saveMovie(any())
     }
 
     @Test
